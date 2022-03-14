d29f41c3b37f500540cc6def90ee45c1ca19f5f8
WILVER XIA IXCOT <WILVER--@hotmail.com>
2022-03-14 17:21:53 -0600

    Avance Del LAboratorio  7

---
+++ Propiedad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_7
+{
+    internal class Propiedad
+    {
+        public string NumeroDeCasa { get; set; }
+        public string DPIDueño { get; set; }
+        public decimal Cuota { get; set; }
+
+    }
+}
